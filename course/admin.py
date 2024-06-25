dc8e23c48238e649d83356203203ada2446167cb
pangxilu <archiepang@163.com>
2024-06-25 10:42:59 +0800

    [fix]: django.core.exceptions.FieldError: 'create_time' cannot be specified for CourseModel model form as it is a non-editable field. Check fields/fieldsets/exclude attributes of class CourseAdmin.

--- course/admin.py
+++ course/admin.py
@@ -5,12 +5,12 @@ from .models import CourseModel
 
 @admin.register(CourseModel) # 使用装饰器注册Course模型
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ['name', 'introduction', 'teacher', 'price', 'create_time', 'update_time'] # 显示的字段
-    search_fields = ['name', 'introduction', 'teacher', 'price'] # 搜索的字段
-    list_filter = ['name', 'teacher', 'price'] # 过滤的字段
+    list_display = ['name', 'introduction', 'teacher', 'price'] # 显示的字段
+    search_fields = list_display # 搜索的字段
+    list_filter = list_display # 过滤的字段
     list_per_page = 2 # 每页显示的数量
     ordering = ['price'] # 按照价格升序排列
     fieldsets = [
         ('基本信息', {'fields': ['name', 'introduction', 'teacher', 'price']}),
-        ('时间信息', {'fields': ['create_time', 'update_time']}),
+
     ] # 在admin后台管理页面显示的字段分组
