f48e4f17882e1f0e93e3a89f0d22cb1cf1ecc430
pangxilu <archiepang@163.com>
2024-06-25 22:30:42 +0800

    [feature]: docs which is the better docs for API Views

--- drf_tutotial_brucepang/urls.py
+++ drf_tutotial_brucepang/urls.py
@@ -19,13 +19,15 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views # 提供token认证的视图
 from rest_framework.schemas import get_schema_view # 视图概要(类似于swagger)
+from rest_framework.documentation import include_docs_urls # 文档
 
-schema_view = get_schema_view(title="DRF API文档", description="DRF API文档", version="1.0.0")
+# schema_view = get_schema_view(title="DRF API文档", description="DRF API文档", version="1.0.0")
 
 urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token), # 获取token的接口
     path('api-auth/', include('rest_framework.urls')), # DRF的登录退出
     path('admin/', admin.site.urls),
     path('course/', include('course.urls')), # course的路由
-    path('schema/', schema_view), # schema视图(相当于swagger)
+   # path('schema/', schema_view), # schema视图(相当于swagger)
+    path('docs/', include_docs_urls(title="DRF API文档", description="DRF API文档")), # 文档
 ]
