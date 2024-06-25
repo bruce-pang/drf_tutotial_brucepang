bcfafd293cf375c58024f85096879439bb33f4a3
brucepang <brucepang0618@gmail.com>
2024-06-25 08:41:58 +0800

    Init

---
+++ course/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import Course
+#注册Course模型，这样在admin后台管理页面就可以看到Course模型，并对其进行增删改查操作
+
+@admin.register(Course) # 使用装饰器注册Course模型
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'introduction', 'teacher', 'price', 'create_time', 'update_time'] # 显示的字段
+    search_fields = ['name', 'introduction', 'teacher', 'price'] # 搜索的字段
+    list_filter = ['name', 'teacher', 'price'] # 过滤的字段
+    list_per_page = 2 # 每页显示的数量
+    ordering = ['price'] # 按照价格升序排列
+    fieldsets = [
+        ('基本信息', {'fields': ['name', 'introduction', 'teacher', 'price']}),
+        ('时间信息', {'fields': ['create_time', 'update_time']}),
+    ] # 在admin后台管理页面显示的字段分组
---
+++ course/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.conf import settings # 导入settings模块
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=255, unique=True, help_text='课程名称', verbose_name='课程名称') # help_text是在admin后台管理页面显示的帮助文本
+    introduction = models.TextField(help_text='课程介绍', verbose_name='课程介绍')
+    teacher = models.ForeignKey(settings.AUTH_USER_MODEL, # 通过settings.AUTH_USER_MODEL来获取用户模型
+                                on_delete=models.CASCADE, help_text='授课老师', verbose_name='授课老师')
+    price = models.DecimalField(max_digits=6, decimal_places=2, help_text='课程价格', verbose_name='课程价格')
+    create_time = models.DateTimeField(auto_now_add=True, help_text='创建时间', verbose_name='创建时间')
+    update_time = models.DateTimeField(auto_now=True, help_text='更新时间', verbose_name='更新时间')
+
+    class Meta:
+        verbose_name = '课程信息'
+        verbose_name_plural = verbose_name # verbose_name_plural是verbose_name的复数形式
+        ordering = ['price'] # 按照价格升序排列
+    def __str__(self):
+        return self.name # 对于Course对象，返回课程名称，相当于重写了Java的toString()方法
+
+
+
